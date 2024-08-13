d658739b4f650b4152975496d47f7f9f6d8d964b
ControllerAM <controller1313@gmail>
2024-08-13 17:22:40 +0300

    this is untill migration DB

---
+++ eTicket/Models/Cinema.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTicket.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Logo { get; set; }
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public List<Movie> Movies { get; set; }
+
+
+    }
+}
--- eTicket/Program.cs
+++ eTicket/Program.cs
@@ -1,7 +1,14 @@
+using eTicket.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"))
+    );
 
 var app = builder.Build();
 
